44a9b53e49e7cd763033be18ca9fde73fe0275d3
Godwin Alexander <alecgee73@gmail.com>
2020-10-19 05:26:38 +0100

    add new files and folders

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+const express = require('express')
+const mongoose = require('mongoose')
+const PORT = 3000
+
+const app = express()
+
+mongoose.connect(`mongodb://localhost:27017/bearerauth`, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+.then(() => {
+    console.log(`connected to mongodb`)
+})
+.catch(e => {
+    console.log(`an error occurred`)
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(PORT, () => {
+    console.log(`app is running on ${PORT}`)
+})
